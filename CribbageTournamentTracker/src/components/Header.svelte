cdc9e8a5b2f1148419d0974020ee36d501028d03
ajfoggia <ajfoggia@comcast.net>
2023-08-29 19:49:45 -0500

    feat(headerbar): creating headerbar

--- CribbageTournamentTracker/src/components/Header.svelte
+++ CribbageTournamentTracker/src/components/Header.svelte
@@ -1,7 +1,11 @@
 <script>
 	import WebsiteLogo from '../lib/images/cribbage-logo.svg';
+	import BellIcon from '../lib/images/bell.svg';
+	import UserIcon from '../lib/images/user.svg';
+	import ExitIcon from '../lib/images/exit.svg';
 </script>
 
+<!--Header section-->
 <header>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="container-fluid">
@@ -27,13 +31,45 @@
 				<span class="navbar-toggler-icon" />
 			</button>
 		</div>
-		<div class="collapse navbar-collapse" id="navbarNav">
+		<!--Navigation icons-->
+		<div class="collapse navbar-collapse justify-content-end w-100" id="navbarNav">
+			<form class="d-flex" role="search">
+				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+				<button class="btn btn-outline-success me-2" type="submit">Search</button>
+			</form>
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="/">Home</a>
+					<a class="nav-link active" href="/">
+						<img
+							src={BellIcon}
+							alt="Bell Logo"
+							width="30"
+							height="24"
+							class="d-inline-block pe-2"
+						/>
+					</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="/about">About</a>
+					<a class="nav-link active" href="/">
+						<img
+							src={UserIcon}
+							alt="User Logo"
+							width="30"
+							height="24"
+							class="d-inline-block pe-2"
+						/>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link active" href="/">
+						<img
+							src={ExitIcon}
+							alt="Exit Logo"
+							width="30"
+							height="24"
+							class="d-inline-block pe-2"
+						/>
+					</a>
 				</li>
 			</ul>
 		</div>
